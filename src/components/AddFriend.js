55aaab22b2850e7f7ac8548432dccb3ef690302e
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2019-02-07 09:58:55 -0500

    works

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -27,17 +27,17 @@ class AddFriend extends Component {
             })
         })
 
-        // fetch('http://localhost:3000/email', {
-        //   method:'POST',
-        //   headers:{
-        //       'Accept': 'application/json',
-        //       'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //       recieverId: foundUser.id,
-        //       senderId: this.props.currentUser.id
-        //     })
-        // })
+        fetch('http://localhost:3000/email', {
+          method:'POST',
+          headers:{
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              recieverId: foundUser.id,
+              senderId: this.props.currentUser.id
+            })
+        })
 
         alert(`You sent a friend request to ${foundUser.name}` )
 
@@ -52,6 +52,7 @@ class AddFriend extends Component {
       <div>
       <h1>Add Friend</h1>
       <input onChange={this.handleChange} type="text" placeholder="add friend"></input>
+    
       <button onClick={this.handleClick} >Submit</button>
 
       </div>
